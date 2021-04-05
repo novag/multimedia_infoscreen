318f1af230661b16fede8532e8857a956ee6dc23
Etienne Fieg <esieg@live.de>
2021-04-05 20:01:34 +0200

    Add YouTube module

--- selector/modules/utils.py
+++ selector/modules/utils.py
@@ -2,8 +2,20 @@ import json
 import os
 import requests
 import socket
-import threading
 
+
+def get_primary_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+
+    return ip
 
 def download_file(name, url):
     res = requests.head(url)
